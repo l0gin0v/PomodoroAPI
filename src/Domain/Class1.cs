9d70df1d5564f7b8713b95f08762195ef7b758e3
l0gin0v <maximlogs2@gmail.com>
2026-06-28 17:59:58 +0500

    add basic structure of projects and build csproj dependencies

---
+++ src/Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Domai;
+
+public class Class1
+{
+}
---
+++ tests/Infrastructure.IntegraionTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.IntegraionTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
